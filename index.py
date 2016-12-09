a981228aa1309740f5c9f986d12360c7d746d8cc
jamesaud <jamaudre@indiana.edu>
2016-12-08 20:06:53 -0500

    updated test

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 from part4.TextCleaner import _stemm, _remove_stopwords
 
-i = input("Input:")
-
-print(_stemm(_remove_stopwords(i)))
+words = 'hello this is a phrase that is going to be parsed by nltk'.split()
+print(_stemm(_remove_stopwords(words)))
